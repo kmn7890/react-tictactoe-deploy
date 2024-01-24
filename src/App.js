08c853b80762860103f90c12cf6d4cc1d580fb43
Minah Kang <tethys@Minahui-MacBookPro.local>
2024-01-24 12:49:26 +0900

    틱택토 앱 로컬 구현 위한 App.css, App.js, index.css, 등 파일 변경사항 저장

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
 import './App.css';
+import Board from './components/Board';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [history, setHistory] = useState([{squares: Array(9).fill(null)}]); 
+  const [xIsNext, setXIsNext] = useState(true);
+  const [stepNumber, setStepNumber] = useState(0);
+  const currentSquareObj = history[stepNumber];
+
+  const handleClick = (i) => {
+    const newHistory = history.slice(0, stepNumber + 1);
+    const newCurrentSquareObj = newHistory[stepNumber];
+    const newSquares = newCurrentSquareObj.squares.slice();
+
+
+
+    if (calculateWinner(newSquares) || newSquares[i]) {return ;}
+    newSquares[i] = xIsNext? "X" : "O"; 
+    setHistory([...newHistory, {squares: newSquares}]);
+    setXIsNext( prev => !prev);
+
+    setStepNumber(newHistory.length); 
+  }
+
+  const calculateWinner = (squares) => {
+    const lines = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
+
+    for (let i = 0; i < lines.length ; i++){
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+            return squares[a];
+        }
+    }
+    return null;
+  }
+  
+  const winner = calculateWinner(currentSquareObj.squares);
+
+  let gamestatus; 
+  if (winner) {
+    gamestatus = `Winner is ` + winner;
+    } else { gamestatus = `Next player : ${xIsNext? "X" : "O"}`}
+  
+  
+  const moves = history.map((stepSqauresObj, move) => {
+    const desc = move? `Go to move #${move}` : "Go to game start";
+    return (
+        <li key = {move}>
+         <button className= "move-button" onClick={()=>jumpTo(move)}>{desc}</button>
+        </li>
+    )
+  })
+
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setXIsNext((step % 2) === 0);
+  }
+
+  return(
+    <div className = 'board-container'>
+      <div className = 'board'>
+        <Board handleClick = {handleClick} squares = {currentSquareObj.squares} />
+      </div>
+      <div className = 'board-info'>
+      <div className = 'gamestatus'>
+         {gamestatus}
+      </div>
+        <ol style = {{listStyle: 'none'}}>
+          {moves}
+        </ol>
+      </div>
     </div>
   );
 }
